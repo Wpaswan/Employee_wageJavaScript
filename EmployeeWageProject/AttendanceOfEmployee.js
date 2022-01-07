2d6acc3bc21ce0e53f7e445cf8fb6022fba214a7
Wpaswan <dablumcapaswan@gmail.com>
2022-01-06 23:50:32 -0800

    [I modified the code to calculate daily wage of employee according to the work]

---
+++ EmployeeWageProject/AttendanceOfEmployee.js
@@ -0,0 +1,11 @@
+const Is_Absent=0
+let empCheck=Math.floor(Math.random()*10)%2
+if(empCheck==Is_Absent)
+{
+    console.log("Employee is absent!")
+}
+else
+{
+    console.log("Employee is present!!")
+
+}
